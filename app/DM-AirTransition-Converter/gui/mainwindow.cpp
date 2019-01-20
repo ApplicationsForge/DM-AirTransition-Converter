90aa463f16917047276e550b0b0705e89cb2929c
Lyanguzov Aleksandr <xtail1996@yandex.ru>
2019-01-20 17:02:19 +0300

    initial

---
+++ app/DM-AirTransition-Converter/gui/mainwindow.cpp
@@ -0,0 +1,22 @@
+#include "mainwindow.h"
+#include "ui_mainwindow.h"
+
+MainWindow::MainWindow(QWidget *parent) :
+    QMainWindow(parent),
+    ui(new Ui::MainWindow)
+{
+    ui->setupUi(this);
+    ui->mainToolBar->hide();
+
+    //Router& router = Router::getInstance();
+
+    // установка оформления statusBar
+    ui->statusBar->setStyleSheet("background-color: #333; color: #33bb33");
+    ui->statusBar->setFont(QFont("Consolas", 14));
+    ui->statusBar->showMessage(tr("State: ready 0123456789"));
+}
+
+MainWindow::~MainWindow()
+{
+    delete ui;
+}
